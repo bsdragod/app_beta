3b0808af478a1aafd10be24c88733cbc4b3b5b8f
Juan <juanmasoluna@gmail.com>
2026-03-25 08:35:43 -0500

    avance

--- src/signup.php
+++ src/signup.php
@@ -4,10 +4,11 @@ include('../config/database.php');
 $f_name = $_POST ['fname'];
 $l_name = $_POST ['lname'];
 $e_mail = $_POST ['email'];
-$m_phone= $_POS  ['mphone'];
-$passwd = $_POST ['passwd'];
+$m_phone= $_POST  ['mphone'];
+$passwd = $_POST ['passwd']; 
+$enc_pass = md5($passwd);
 //query to insert into SQL.
-$sql = "INSERT INTO users (nombre,apellido,email,telefono,contraseña) VALUES ('$f_name','$lname','$e_mail','$m_phone','$passwd')";
+$sql = "INSERT INTO users (nombre,apellido,email,telefono,contraseña) VALUES ('$f_name','$l_name','$e_mail','$m_phone','$enc_pass')";
 //execute query
 pg_query($sql);
 ?>
